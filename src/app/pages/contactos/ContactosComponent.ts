30508ea606ed0b7afed15fefaa4f5b6353bc6715
jguzman1731 <jguzman1731@gmail.com>
2024-08-12 21:08:25 -0500

    crear

--- src/app/pages/contactos/ContactosComponent.ts
+++ src/app/pages/contactos/ContactosComponent.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-contactos',
   standalone: true,
@@ -8,5 +9,14 @@ import { Component } from '@angular/core';
   styleUrl: './contactos.component.css'
 })
 export class ContactosComponent {
+  Nombre: string | undefined;
+  email: string | undefined;
+  Mensaje : string | undefined;
 
+  constructor() { }
+
+  onSubmit() {
+    console.log('Form submitted!');
+  }
 }
+
