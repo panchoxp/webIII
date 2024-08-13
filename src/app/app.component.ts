2b59b695b764048e4db725e166fc7ca7fccbfc4d
panchoxp <francisco_smfd@hotmail.es>
2024-08-12 22:03:02 -0500

    debugger

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {NavBarComponent} from
+import { NavBarComponent } from './component/nav-bar/nav-bar.component';
+import { ContactosComponent } from "./pages/contactos/ContactosComponent";
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavBarComponent],
+  imports: [RouterOutlet, NavBarComponent, ContactosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { NosotrosComponent } from './pages/nosotros/nosotros.component';
+import { OfertasComponent } from './pages/ofertas/ofertas.component';
+import { TerminosyCondicionesComponent } from './pages/terminosy-condiciones/terminosy-condiciones.component';
+import { ContactosComponent } from './pages/contactos/ContactosComponent';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: 'nosotros', component: NosotrosComponent},
+    {path: 'ofertas', component: OfertasComponent},
+    {path: 'terminos', component: TerminosyCondicionesComponent},
+    {path: 'contactos', component: ContactosComponent},
+
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+];
